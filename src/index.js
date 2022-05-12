86cee3e3c4026fabb5bb2b88d255535666497c3b
orlandouchiha425 <valadez425@gmail.com>
2022-05-12 19:48:20 -0400

    testing commits

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './pages/App/App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+   <Router>
+     
+     <App />
+     
+    </Router> 
+  </React.StrictMode>,
+  document.getElementById('root')
 );
---
+++ src/pages/Homepage.js
@@ -0,0 +1,10 @@
+
+
+export default function HomePage(){
+
+    return(
+        <main>
+            <h1>This is the HomePage</h1>
+        </main>
+    )
+}
